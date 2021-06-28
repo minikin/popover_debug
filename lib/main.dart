2a01bcacc8a41502556150c9be97fea9ea8284e1
minikin <djminikin@gmail.com>
2021-06-28 16:29:18 +0200

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:popover/popover.dart';
 
 void main() => runApp(App());
 
@@ -8,13 +8,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ManageDocuments());
+    return MaterialApp(
+      home: ManageDocuments(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
 class ManageDocuments extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const centerKey = ValueKey<String>('bottom-sliver-list');
     return CustomScrollView(
       physics: const BouncingScrollPhysics(),
       slivers: [
@@ -24,26 +28,52 @@ class ManageDocuments extends StatelessWidget {
           backgroundColor: Theme.of(context).scaffoldBackgroundColor,
           title: Text(
             'Manage Documents',
-            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            style: TextStyle(color: Colors.black),
           ),
           centerTitle: true,
           actions: [
             IconButton(
               icon: Icon(
                 Icons.add,
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Colors.black,
               ),
-              onPressed: () => showPopover(
+              onPressed: () => showCupertinoModalPopup<void>(
                 context: context,
-                bodyBuilder: (context) => Column(
-                  children: [
-                    Text('Text One'),
-                    Text('Text Two'),
-                    Text('Text Three'),
+                builder: (context) => CupertinoActionSheet(
+                  title: const Text('Title'),
+                  message: const Text('Message'),
+                  actions: [
+                    CupertinoActionSheetAction(
+                      child: const Text('Action One'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    CupertinoActionSheetAction(
+                      child: const Text('Action Two'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    )
                   ],
                 ),
-                direction: PopoverDirection.bottom,
               ),
+
+              // onPressed: () => showPopover(
+              //   context: context,
+              //   bodyBuilder: (context) => SafeArea(
+              //     child: Container(
+              //       color: Colors.red,
+              //       child: Column(
+              //         children: [
+              //           Text('Text One'),
+              //           Text('Text Two'),
+              //           Text('Text Three'),
+              //         ],
+              //       ),
+              //     ),
+              //   ),
+              // ),
             ),
           ],
         ),
