fa7405d2ade5470ce1979c136209bccbddc4fb57
minikin <djminikin@gmail.com>
2021-07-26 09:49:48 +0200

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
 
 void main() => runApp(App());
 
@@ -18,7 +19,6 @@ class App extends StatelessWidget {
 class ManageDocuments extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const centerKey = ValueKey<String>('bottom-sliver-list');
     return CustomScrollView(
       physics: const BouncingScrollPhysics(),
       slivers: [
@@ -32,52 +32,80 @@ class ManageDocuments extends StatelessWidget {
           ),
           centerTitle: true,
           actions: [
-            IconButton(
-              icon: Icon(
-                Icons.add,
-                color: Colors.black,
-              ),
-              onPressed: () => showCupertinoModalPopup<void>(
-                context: context,
-                builder: (context) => CupertinoActionSheet(
-                  title: const Text('Title'),
-                  message: const Text('Message'),
-                  actions: [
-                    CupertinoActionSheetAction(
-                      child: const Text('Action One'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    CupertinoActionSheetAction(
-                      child: const Text('Action Two'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    )
-                  ],
-                ),
-              ),
+            PopMe(),
+            // IconButton(
+            //   icon: Icon(
+            //     Icons.add,
+            //     color: Colors.black,
+            //   ),
+            // onPressed: () => showCupertinoModalPopup<void>(
+            //   context: context,
+            //   builder: (context) => CupertinoActionSheet(
+            //     title: const Text('Title'),
+            //     message: const Text('Message'),
+            //     actions: [
+            //       CupertinoActionSheetAction(
+            //         child: const Text('Action One'),
+            //         onPressed: () {
+            //           Navigator.pop(context);
+            //         },
+            //       ),
+            //       CupertinoActionSheetAction(
+            //         child: const Text('Action Two'),
+            //         onPressed: () {
+            //           Navigator.pop(context);
+            //         },
+            //       )
+            //     ],
+            //   ),
+            // ),
 
-              // onPressed: () => showPopover(
-              //   context: context,
-              //   bodyBuilder: (context) => SafeArea(
-              //     child: Container(
-              //       color: Colors.red,
-              //       child: Column(
-              //         children: [
-              //           Text('Text One'),
-              //           Text('Text Two'),
-              //           Text('Text Three'),
-              //         ],
-              //       ),
-              //     ),
-              //   ),
-              // ),
-            ),
+            //   onPressed: () => showPopover(
+            //     context: context,
+            //     bodyBuilder: (context) => SafeArea(
+            //       child: Container(
+            //         color: Colors.red,
+            //         child: Column(
+            //           children: [
+            //             Text('Text One'),
+            //             Text('Text Two'),
+            //             Text('Text Three'),
+            //           ],
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class PopMe extends StatelessWidget {
+  const PopMe({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.add,
+        color: Colors.black,
+      ),
+      onPressed: () => showPopover(
+        context: context,
+        bodyBuilder: (context) => Container(
+          color: Colors.red,
+          child: Column(
+            children: [
+              Text('Text One'),
+              Text('Text Two'),
+              Text('Text Three'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
